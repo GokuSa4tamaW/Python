889ea7fbf9a13440874e66a34e323fbde341de6e
Mauricio Souza <mamaubritosss@gmail.com>
2022-07-29 14:40:03 -0300

    commit

---
+++ Exercícios000/Exércicio001.py
@@ -0,0 +1,21 @@
+#Hoje, iremos tratar sobre a função print! A maioria já deve saber o que essa funçao faz. 
+#Sendo uma função interna do Pyhton ela necessita de ().
+#O print pode imprimi para os dados que há dentro dos paratenses por exemplo.
+
+print('Estou na faculdade agora!')
+
+#Caso você rode o programa ele irá exibir, a mensagem: Estou na faculdade agora! Sem aspas, 
+# pois tanto as aspas simples como as duplas servem para defiinir uma str()
+
+print("Estou na faculdade agora!")
+
+#Se atribuirmos, por exemplo, uma váriavel chamada status a uma string o que será que acontece?
+
+status = 'Estou na faculdade agora!' 
+print(status)
+
+#Aconteceu o esperado, o sinal =, na linguagem Pyhton significa atribuição, sendo assim,
+#podemos atribuir valores inteiros{int()}, valores reais{float()}, strings{str()} entre outros!
+
+#Focarei na resolução de exercícios simples, moderados e difícies. A teoria pode ser
+#aprendida por vários cursos online. Então, terá um foco para resolução!
